e009880a62f7398af67bf61ccb2eaf368139eff0
jvallexm <jvallexm@gmail.com>
2022-07-18 14:43:57 -0400

    F5 external now has gw attached

--- main.tf
+++ main.tf
@@ -113,7 +113,7 @@ module "subnets" {
           cidr     = local.subnet_tiers["zone-${zone}"][tier]
           acl_name = "edge-acl"
           public_gateway = (
-            tier != "bastion" && tier != "edge"
+            tier != "bastion" && tier != "f5-external"
             ? false
             : true
           )
